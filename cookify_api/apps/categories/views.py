20e9f5920ca0ebae92b2b9e24b7a7e2fe5942fb6
Maira Berlato <mairaberlatof@gmail.com>
2023-04-12 17:21:00 -0300

    category endpoint

--- cookify_api/apps/categories/views.py
+++ cookify_api/apps/categories/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Category
+from .serializers import CategorySerializer
+
+class CategoryListView(generics.ListAPIView):
+  queryset = Category.objects.all()
+  serializer_class = CategorySerializer
