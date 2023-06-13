fa7ad1df6fcabe5a6fd244a644529a3712bb1500
Maira Berlato <mairaberlatof@gmail.com>
2023-06-13 18:40:44 -0300

    debounce filter

--- cookify-app/app/components/recipes/RecipesSearchBar/index.tsx
+++ cookify-app/app/components/recipes/RecipesSearchBar/index.tsx
@@ -11,21 +11,24 @@ import { RecipesSearchBarProps } from './types';
 export const RecipesSearchBar: React.FC<RecipesSearchBarProps> = ({ onClickFilter }) => {
   const resetPage = useResetRecoilState(recipePageState);
   const [filterParams, setFilterParams] = useRecoilState(recipeFiltersState);
+  const [value, setValue] = React.useState(filterParams?.filter);
 
   const setFilter = (filter?: string) => {
     setFilterParams({ ...filterParams, filter });
     resetPage();
   };
 
+  React.useEffect(() => {
+    const handler = setTimeout(() => {
+      if (filterParams?.filter !== value) setFilter(value);
+    }, 1000);
+    return () => clearTimeout(handler);
+  }, [value]);
+
   return (
     <SearchBarContainer>
       <InputContainer>
-        <TextInput
-          placeholder="Buscar..."
-          leadingIcon="search"
-          value={filterParams?.filter}
-          onChange={setFilter}
-        />
+        <TextInput placeholder="Buscar..." leadingIcon="search" value={value} onChange={setValue} />
       </InputContainer>
       <IconButton color="gray" name="tune" onPress={onClickFilter} />
     </SearchBarContainer>
